01aa02ff0099448da0e7f621bffdd0fe07aa5bf4
dianbester-JJ <dianbester1@gmail.com>
2026-05-05 06:18:47 +0200

    increase logo size

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -51,7 +51,7 @@ const AppSidebar = () => {
             <img
               src={jobjetLogo}
               alt="JobJet"
-              className={collapsed ? "h-10 w-auto" : "h-16 w-auto"}
+              className={collapsed ? "h-20 w-auto" : "h-32 w-auto"}
             />
           </Link>
         </SidebarHeader>
